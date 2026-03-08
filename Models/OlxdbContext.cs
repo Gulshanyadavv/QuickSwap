40b72d7a7bb2036dc13b83ed5d81b694e26f45f3
Gulshan Yadav <gulshanyyy53@gmail.com>
2026-03-08 19:22:30 +0530

    Fix: allow connection string override from env vars

--- Models/OlxdbContext.cs
+++ Models/OlxdbContext.cs
@@ -34,8 +34,12 @@ public partial class OlxdbContext : DbContext
     public virtual DbSet<UserAdActivity> UserAdActivities { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-LLKF91PS\\SQLEXPRESS;Database=OLXDB;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=True;");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-LLKF91PS\\SQLEXPRESS;Database=OLXDB;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=True;");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
